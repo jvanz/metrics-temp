87455ece5de4528aa8914689904204ce69637731
Rafael Fernández López <ereslibre@ereslibre.es>
2021-10-27 18:00:15 +0200

    Update

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releas
 kubectl wait --for=condition=Available deployment --timeout=2m -n opentelemetry-operator-system --all
 helm install --wait --create-namespace --namespace prometheus --values prometheus-values.yaml prometheus prometheus-community/kube-prometheus-stack
 helm install --wait --namespace kubewarden kubewarden-crds helm-charts/charts/kubewarden-crds
-helm install --wait --namespace kubewarden kubewarden-controller helm-charts/charts/kubewarden-controller
+helm install --wait --namespace kubewarden --values kubewarden-values.yaml kubewarden-controller helm-charts/charts/kubewarden-controller
 kubectl apply -f - <<EOF
 apiVersion: policies.kubewarden.io/v1alpha2
 kind: PolicyServer
